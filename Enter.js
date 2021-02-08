86f1f60f6dabcdb17ac3bd639492b6284a91e652
AtharvaVanam <70266377+AtharvaVanam@users.noreply.github.com>
2021-02-08 17:33:42 +0530

    Add files via upload

---
+++ Enter.js
@@ -0,0 +1,19 @@
+class Enter {
+  constructor(x, y, width, height) {
+    var options = {
+        isStatic:true
+        
+    }
+    this.wall = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.wall);
+  }
+  display(){
+    var pos =this.wall.position;
+    rectMode(CENTER);
+    fill("red");
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+};
